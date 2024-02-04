9e64d5f581f9c718acb206d8d556bcbddb4c9af4
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-04 11:19:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,6 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 FILDLT_CNL = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('FILDLT_CNL', '0').split()]
-START_MESSAGE = environ.get('START_MESSAGE', script.START_TXT)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
