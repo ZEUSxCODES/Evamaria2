d194de01124c70f1927d5271d49785b92da6874d
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-06 14:55:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('FilmNest', url='https://t.me/Film_Nest')] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
